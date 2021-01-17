65a914fbf6ae714adb941a8e815f5e9c1d8daab5
AnaAlvarezA <ana.alvarez@ingetega.com>
2021-01-17 13:54:15 +0100

    listo

--- laboratorio.js
+++ laboratorio.js
@@ -1,10 +1,14 @@
 function mostrarValoracion() {
-    let valor = Number(document.getElementById("rango").value)
+    let valor = Number(document.getElementById("rango").value); /* document.getElementById("rango").value devuelve un string, 
+                                                                que, en este caso, hay que convertir a number, para tener un valor numérico */
+    
     alert("Has valorado con "+ valor +" puntos");
- };
+ }
 
 function mostrarCuenta(){
-    let cPais, cIban, cEntidad, cSucursal, cDc, numeroCuenta;
+    let cPais, cIban, cEntidad, cSucursal, cDc, numeroCuenta; // considero, que como string, puedo implementar la funcionalidad requerida 
+    
+    // se recogen todos los valores introducidos por el usuario
     cPais = document.getElementById("pais").value;
     cIban = document.getElementById("iban").value;
     cEntidad = document.getElementById("entidad").value;
@@ -15,15 +19,15 @@ function mostrarCuenta(){
     alert("Le informamos que su cuenta bancaria es: "+ cPais+cIban+"-"+cEntidad+"-"+cSucursal+"-"+cDc+"-"+numeroCuenta);
 }
     
-
 function mostrarDiaSemana(){
-let fecha = document.getElementById("fecha").value;
-let fechaDia = new Date(fecha);
-let dia = fechaDia.getDate();
-let diaSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado");
 
-alert("La fecha seleccionada en el elemnto de fecha es un "+ diaSemana[dia+1]);
-}; 
+    let fecha = document.getElementById("fecha").value; //se recoge la fecha elegida por el usuario
+    let fechaDia = new Date(fecha); // se cambia la fecha de formato, para disponer del día de la semana
+    let dia = fechaDia.getDay(); //devuelve el número del día de la semana: desde 0 (domingo) hasta 6 (sábado)
+    let diaSemana = new Array("Domingo","Lunes","Martes","Miércoles","Jueves","Viernes","Sábado"); // valores de lo días de la semana
+
+    alert("La fecha seleccionada en el elemento de fecha es un "+ diaSemana[dia]);
+}
 
 
  
